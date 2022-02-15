98f7c2391f90584caec7e7d0b3c925d8a065e02c
unknown <sumit.b55555@gmail.com>
2022-02-15 12:52:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+/* eslint-disable react/jsx-no-undef */
 import './App.css';
+//import LandingScreen from './Component/LandingScreen';
+import Login from './login';
+import Navbar from './Component/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+      <Navbar title="Travelgram.com" />
+     <BrowserRouter>
+      <Route path="/login" exact component={Login} />
+      {/* <Route path="/" exact component={LandingScreen}/> */}
+      </BrowserRouter>
+      </div>    
   );
+
 }
 
 export default App;
---
+++ src/Component/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Navbar(props) {
+    return (
+        <nav className="navbar navbar-dark bg-primary">
+            <div class="container-fluid">
+                <img src="/travelmanagement/src/TRAVEL.jpg" alt="" width="40" height="30" class="d-inline-block align-text-top" />
+                <a class="navbar-brand" href="/">{props.title}</a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Travel Package</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Flight Booking</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Hotel Booking</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown link
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="/">Action</a></li>
+                                <li><a class="dropdown-item" href="/">Another action</a></li>
+                                <li><a class="dropdown-item" href="/">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
