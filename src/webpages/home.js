15352d5e3f06a3c207cf6cfd08e5b2c8868f128f
unknown <sumit.b55555@gmail.com>
2022-02-16 00:52:32 +0530

    added home page

--- src/webpages/home.js
+++ src/webpages/home.js
@@ -4,7 +4,7 @@ import background from "../img/1386029.jpg";
 export default function Home() {
     return (
         <div style= {{backgroundImage: `url(${background})`, backgroundSize:'contain',height:1010,width:1345}}>
-            
+            <h1>Welcome to Travelgram.com</h1>
         </div>
     );
 }
